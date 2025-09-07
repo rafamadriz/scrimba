d055e55f236d8fefd190ccc9c1d373d739bbace7
Rafael Madriz <rafa@rafaelmadriz.com>
2025-09-07 15:47:57 -0300

    fix(password-generator): remove unused copy button

--- 03-javascript-fundamentals/02-password-generator/index.js
+++ 03-javascript-fundamentals/02-password-generator/index.js
@@ -3,7 +3,6 @@ const lowercase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "
 const numbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
 const symbols = ["~", "`", "!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "_", "-", "+", "=", "{", "[", "}", "]", ",", "|", ":", ";", "<", ">", ".", "?", "/"];
 
-
 function PasswordSettingsGenerator(symbols, numbers, uppercase, lowercase) {
     const settingsEl = document.getElementsByClassName("settings-toggles");
 
@@ -42,7 +41,6 @@ function chooseRandomIndex() {
     const setting = chooseRandomSetting();
     return setting[randomNumber(setting.length)];
 }
-
 
 function generatePassword() {
     const passwdLengthFromEl = document.getElementById("passwd-length")
